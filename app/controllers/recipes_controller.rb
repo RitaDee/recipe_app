441a0dbd9b0032fe014011d06b81d20345e28317
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-05-30 11:46:08 +0100

    Create shopping_list controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,7 +7,9 @@ class RecipesController < ApplicationController
   end
 
   # GET /recipes/1 or /recipes/1.json
-  def show; end
+  def show
+    @r_foods = @recipe.r_foods.includes(:food)
+  end
 
   # GET /recipes/new
   def new
