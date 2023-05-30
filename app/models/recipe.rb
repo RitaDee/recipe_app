32a15eeb5041600831de010678d004dc6ff86ae1
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-05-30 08:26:35 +0100

    Add validation to recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,11 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+
+  has_many :recipe_foods, dependent: :destroy
+  has_many :foods, through: :recipe_foods
+
+  validates :name, presence: true, length: { maximum: 250 }
+  validates :description, presence: true
+  validates :preparation_time, presence: true, numericality: { greater_than_or_equal_to: 0.0 }
+  validates :cooking_time, presence: true, numericality: { greater_than_or_equal_to: 0.0 }
 end
